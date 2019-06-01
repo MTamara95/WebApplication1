b1161b5d57efc0290b6e94db536663f785e964b8
MTamara95 <tamaramarcetic@hotmail.com>
2019-06-02 00:14:10 +0200

    Implemented determining FullName whenever FirstName and LastName are inserted or changed.

---
+++ WebApplication1/Controllers/AboutController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AboutController : Controller
+    {
+        public string Create(string firstname, string lastname)
+        {
+            List<string> tmpUsernames = new List<string>(new string[] { "tmarcetic", "mnesic", "nilic", "lmarkovic", "vkovacevic",
+            "tmarkovic", "tamarkovic", "tammarkovic", "tamamarkovic", "tamarmarkovic", "tamaramarkovic"});
+
+            string username = "";
+            int endIndex = 1;
+
+            while (endIndex < firstname.Length)
+            {
+                if (!tmpUsernames.Contains(firstname.Substring(0, endIndex).ToLower() + lastname.ToLower()))
+                {
+                    username = firstname.Substring(0, endIndex).ToLower() + lastname.ToLower();
+                    break;
+                }
+                endIndex++;
+            }
+
+            if (username == "")
+            {
+                int num = 1;
+                while (true)
+                {
+                    if (!tmpUsernames.Contains(firstname.Substring(0, endIndex - 1).ToLower() + lastname.ToLower() + num.ToString()))
+                    {
+                        username = firstname.Substring(0, 1).ToLower() + lastname.ToLower() + num.ToString();
+                        break;
+                    }
+                    num++;
+                }
+            }
+
+            return username;
+        }
+
+    }
+}
---
+++ WebApplication1/Scripts/about.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    $("#firstname").focus();
+
+    $("#firstname").change(function (e) {
+        GenerateUsername();
+    });
+
+    $("#lastname").change(function (e) {
+        GenerateUsername();
+    });
+
+    function GenerateUsername() {
+        var fullname = { firstname: $("#firstname").val(), lastname: $("#lastname").val() };
+
+        $.ajax({
+            url: '/About/Create',
+            data: fullname,
+            success: function (data) {
+                document.getElementById('username').value = data;
+            },
+            error: function () {
+                alert("error");
+            }
+        });
+    }
+
+});
